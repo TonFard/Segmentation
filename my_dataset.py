2142c34de9034adc0f83c9174936dd10db3c9d71
TonFard <77236666+TonFard@users.noreply.github.com>
2023-03-22 14:06:38 +0800

    修改bug

--- my_dataset.py
+++ my_dataset.py
@@ -73,3 +73,11 @@ class MyDataset(data.Dataset):
         batched_imgs = cat_list(images, fill_value=0) # 将图像进行批处理
         batched_targets = cat_list(targets, fill_value=255) # 将掩码进行批处理
         return batched_imgs, batched_targets
+
+def cat_list(images, fill_value=0):
+    max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
+    batch_shape = (len(images),) + max_size
+    batched_imgs = images[0].new(*batch_shape).fill_(fill_value)
+    for img, pad_img in zip(images, batched_imgs):
+        pad_img[..., :img.shape[-2], :img.shape[-1]].copy_(img)
+    return batched_imgs
